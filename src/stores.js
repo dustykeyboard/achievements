af519bb50a67abd738e63cde569b1607e02945f0
Philip Boardman <dustykeyboard@outlook.com.au>
2022-02-18 22:19:42 +1100

    Add application state persistance with localStorage

--- src/stores.js
+++ src/stores.js
@@ -1,10 +1,19 @@
+import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 import { sampleWorkAreas, sampleAchievements } from './sample.js';
 
-export let view = writable('summary');
+const initialView = browser ? window.localStorage.getItem('view') ?? 'summary' : 'summary';
+export let view = writable(initialView);
+view.subscribe(value => { if (browser) { localStorage.view = value } });
 
-export let user = writable('_you_');
+const initialUser = browser ? window.localStorage.getItem('user') ?? 'User' : 'User';
+export let user = writable(initialUser);
+user.subscribe(value => { if (browser) { localStorage.user = value } });
 
-export let workAreas = writable(sampleWorkAreas);
+const initialWorkAreas = browser ? JSON.parse(window.localStorage.getItem('workAreas')) ?? sampleWorkAreas : sampleWorkAreas;
+export let workAreas = writable(initialWorkAreas);
+workAreas.subscribe(value => { if (browser) { localStorage.workAreas = JSON.stringify(value) } });
 
-export let achievements = writable(sampleAchievements);
+const initialAchievements = browser ? JSON.parse(window.localStorage.getItem('achievements')) ?? sampleAchievements : sampleAchievements;
+export let achievements = writable(initialAchievements);
+achievements.subscribe(value => { if (browser) { localStorage.achievements = JSON.stringify(value) } });
