c25288df181cdcba36dc2976608345e2e56fd1d1
Philip Boardman <dustykeyboard@outlook.com.au>
2022-02-18 22:44:42 +1100

    Refactor localStorage

--- src/stores.js
+++ src/stores.js
@@ -2,18 +2,14 @@ import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 import { sampleWorkAreas, sampleAchievements } from './sample.js';
 
-const initialView = browser ? window.localStorage.getItem('view') ?? 'summary' : 'summary';
-export let view = writable(initialView);
+export let view = writable(browser && window.localStorage?.view || 'summary');
 view.subscribe(value => { if (browser) { localStorage.view = value } });
 
-const initialUser = browser ? window.localStorage.getItem('user') ?? 'User' : 'User';
-export let user = writable(initialUser);
+export let user = writable(browser && window.localStorage?.user || 'User');
 user.subscribe(value => { if (browser) { localStorage.user = value } });
 
-const initialWorkAreas = browser ? JSON.parse(window.localStorage.getItem('workAreas')) ?? sampleWorkAreas : sampleWorkAreas;
-export let workAreas = writable(initialWorkAreas);
+export let workAreas = writable(browser && JSON.parse(window.localStorage?.workAreas) || sampleWorkAreas);
 workAreas.subscribe(value => { if (browser) { localStorage.workAreas = JSON.stringify(value) } });
 
-const initialAchievements = browser ? JSON.parse(window.localStorage.getItem('achievements')) ?? sampleAchievements : sampleAchievements;
-export let achievements = writable(initialAchievements);
+export let achievements = writable(browser && JSON.parse(window.localStorage?.getItem('achievements')) || sampleAchievements);
 achievements.subscribe(value => { if (browser) { localStorage.achievements = JSON.stringify(value) } });
